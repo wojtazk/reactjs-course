b6df3f23b56a5ca694a775120c7aa39423b416bc
Wojciech Kowal <w.kowal202@gmail.com>
2022-07-27 12:06:10 +0200

    router v5 basic setup

---
+++ 14-react-router/src/App.jsx
@@ -0,0 +1,19 @@
+import { Route } from 'react-router-dom';
+import Products from './components/Products';
+import Welcome from './components/Welcome';
+
+function App() {
+  return (
+    <div>
+      <Route path="/welcome">
+        <Welcome />
+      </Route>
+
+      <Route path="/products">
+        <Products />
+      </Route>
+    </div>
+  );
+}
+
+export default App;
