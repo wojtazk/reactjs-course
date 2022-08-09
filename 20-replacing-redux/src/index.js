cd48c66eaec18b7efe93dc53bc2bf7858f465dd5
Wojciech Kowal <w.kowal202@gmail.com>
2022-08-09 15:10:18 +0200

    fixup

--- 20-replacing-redux/src/index.js
+++ 20-replacing-redux/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import { Provider } from 'react-redux';
-import { combineReducers, createStore } from 'redux';
+// import { combineReducers, createStore } from 'redux';
 import { BrowserRouter } from 'react-router-dom';
 
 import './index.css';
