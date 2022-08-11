e4f3884f454c364022c1e4a17adeffa55e57d8f3
Wojciech Kowal <w.kowal202@gmail.com>
2022-08-11 14:17:43 +0200

    typescript demo

---
+++ 22-typescript-demo/with-typescript.js
@@ -0,0 +1,6 @@
+// @ts-check
+function add_ts(a, b) {
+    return a + b;
+}
+var result_ts = add_ts(2, 5);
+console.log(result_ts);
---
+++ 22-typescript-demo/with-typescript.ts
@@ -0,0 +1,8 @@
+// @ts-check
+function add_ts(a: number, b: number) {
+  return a + b;
+}
+
+const result_ts = add_ts(2, 5);
+
+console.log(result_ts);
